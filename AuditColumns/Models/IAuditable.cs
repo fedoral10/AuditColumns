a13e667bef1b3ff5f1a70878e966d3f856971f6d
Jorge Potosme <fedoral10@gmail.com>
2019-07-01 09:41:47 -0600

    Agregada la interfaz IAuditable

---
+++ AuditColumns/Models/IAuditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuditColumns.Models
+{
+    public interface IAuditable
+    {
+        DateTime FECHA_CREACION { get; set; }
+        string USUARIO_CREACION { get; set; }
+
+        DateTime FECHA_MODIFICACION { get; set; }
+        string USUARIO_MODIFICACION { get; set; }
+    }
+}
