2fa492a6d39fb129aa7818eff18e9cba3bb21627
Jorge Potosme <fedoral10@gmail.com>
2019-07-01 09:44:55 -0600

    Agregada la clase DefaultModel

---
+++ AuditColumns/Models/DefaultModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuditColumns.Models
+{
+    public class DefaultModel : IAuditable
+    {
+        public DateTime FECHA_CREACION { get; set; }
+        public string USUARIO_CREACION { get; set; }
+        public DateTime FECHA_MODIFICACION { get; set; }
+        public string USUARIO_MODIFICACION { get; set; }
+    }
+}
