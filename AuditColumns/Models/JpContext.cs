fc2a898f3591c9cea75fedfc1f39a534fa7c47e4
Jorge Potosme <fedoral10@gmail.com>
2019-07-01 10:28:16 -0600

    agregado el dbcontext y la referencia de EntityFramework

---
+++ AuditColumns/Models/JpContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AuditColumns.Models
+{
+    public class JpContext : DbContext
+    {
+        public DbSet<Articulo> Articulos {get;set;}
+    }
+}
