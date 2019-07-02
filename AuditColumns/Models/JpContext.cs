41a7a440855cbcff7a4114753534b37b78682e3e
Jorge Potosme <fedoral10@gmail.com>
2019-07-02 09:51:41 -0600

    agregado el metodo savechanges

--- AuditColumns/Models/JpContext.cs
+++ AuditColumns/Models/JpContext.cs
@@ -10,5 +10,32 @@ namespace AuditColumns.Models
     {
         public DbSet<Articulo> Articulos {get;set;}
         public JpContext() : base("DefaultConnection") { }
+
+        public override int SaveChanges()
+        {
+            var changeSet = ChangeTracker.Entries<IAuditable>();
+
+            if (changeSet != null)
+            {
+                foreach (var auditableEntry in changeSet)
+                {
+                    if (auditableEntry.State == EntityState.Added || auditableEntry.State == EntityState.Modified)
+                    {
+                        switch (auditableEntry.State)
+                        {
+                            case EntityState.Modified:
+                                auditableEntry.Entity.FECHA_MODIFICACION = DateTime.Now;
+                                auditableEntry.Entity.USUARIO_MODIFICACION = HttpContext.Current.User.Identity.Name;
+                                break;
+                            case EntityState.Added:
+                                auditableEntry.Entity.FECHA_CREACION = DateTime.Now;
+                                auditableEntry.Entity.USUARIO_CREACION = string.IsNullOrEmpty(HttpContext.Current.User.Identity.Name) ? "DevelGuest" : HttpContext.Current.User.Identity.Name;
+                                break;
+                        }
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
     }
 }
