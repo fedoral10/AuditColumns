df01b2acd963b4085fecd765a49f172803925942
Jorge Potosme <fedoral10@gmail.com>
2019-07-02 09:25:05 -0600

    habilitadas las migraciones

---
+++ AuditColumns/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace AuditColumns.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AuditColumns.Models.JpContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(AuditColumns.Models.JpContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
