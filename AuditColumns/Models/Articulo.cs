5e0ba3d23868265e5f8826347b5db224b5355105
Jorge Potosme <fedoral10@gmail.com>
2019-07-02 09:27:21 -0600

    agregado el nombre de la tabla

--- AuditColumns/Models/Articulo.cs
+++ AuditColumns/Models/Articulo.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace AuditColumns.Models
 {
+    [Table("ARTICULO")]
     public class Articulo:DefaultModel
     {
         public string NOMBRE { get; set; }
